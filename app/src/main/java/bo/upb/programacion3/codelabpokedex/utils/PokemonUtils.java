37d8b98a790e3f5bce023ffd51141e2003ba9a31
nickygon <nikita27111@hotmail.com>
2020-06-17 18:40:50 -0400

    17-06. Pokemons y usuario listos!

--- app/src/main/java/bo/upb/programacion3/codelabpokedex/utils/PokemonUtils.java
+++ app/src/main/java/bo/upb/programacion3/codelabpokedex/utils/PokemonUtils.java
@@ -27,6 +27,22 @@ public class PokemonUtils {
         pokemons.add(new Pokemon(10, "Sandshrew", R.drawable.sandshrew, "Ground"));
         pokemons.add(new Pokemon(11, "Venonat", R.drawable.venonat, "Bug"));
         pokemons.add(new Pokemon(12, "Zubat", R.drawable.zubat, "Poison"));
+
+        //Mis pokemon (Si, medio fan de los starters tipo Agua XD)
+        pokemons.add(new Pokemon(13,"Mimikyu",R.drawable.mimikyu,"Ghost/Fairy"));
+        pokemons.add(new Pokemon(14,"Popplio",R.drawable.popplio,"Water"));
+        pokemons.add(new Pokemon(15,"Hydreigon",R.drawable.hydreigon,"Dragon/Dark"));
+        pokemons.add(new Pokemon(16,"Greninja",R.drawable.greninja,"Water/Dark"));
+        pokemons.add(new Pokemon(17,"Bayleef",R.drawable.bayleef,"Grass"));
+        pokemons.add(new Pokemon(18,"Sobble",R.drawable.sobble,"Water"));
+        pokemons.add(new Pokemon(19,"Impidimp",R.drawable.impidimp,"Dark/Fairy"));
+        pokemons.add(new Pokemon(20,"Empoleon",R.drawable.empoleon,"Water/Steel"));
+        pokemons.add(new Pokemon(21,"Goomy",R.drawable.goomy,"Dragon"));
+        pokemons.add(new Pokemon(22,"Oshawott",R.drawable.oshawott,"Water"));
+        pokemons.add(new Pokemon(23,"Drapion",R.drawable.drapion,"Poison/Dark"));
+        pokemons.add(new Pokemon(24,"Marshtomp",R.drawable.marshtomp,"Water/Ground"));
+        pokemons.add(new Pokemon(25,"Yamper",R.drawable.yamper,"Electric"));
+
         return pokemons;
     }
 }
